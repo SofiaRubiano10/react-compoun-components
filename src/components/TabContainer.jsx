3a6eb14e4f31a8e3e7f91285a7d6f36e62116d25
SofiaRubiano10 <10sofiarubiano@gmail.com>
2023-06-24 13:09:06 +0200

    Patrones: Compound Component - Estilo para Tabs

--- src/components/TabContainer.jsx
+++ src/components/TabContainer.jsx
@@ -5,15 +5,17 @@ import TabPanel from "./TabPanel";
 const TabContainer = () => {
   return (
     <TabSwitcher>
-      <Tab id="1">
-        <span>Tab 1</span>
-      </Tab>
-      <Tab id="2">
-        <span>Tab 2</span>
-      </Tab>
-      <Tab id="3">
-        <span>Tab 3</span>
-      </Tab>
+      <div className="tab-container">
+        <Tab id="1">
+          <span>Tab 1</span>
+        </Tab>
+        <Tab id="2">
+          <span>Tab 2</span>
+        </Tab>
+        <Tab id="3">
+          <span>Tab 3</span>
+        </Tab>
+      </div>
       <TabPanel whenActive="1">contenido 1</TabPanel>
       <TabPanel whenActive="2">contenido 2</TabPanel>
       <TabPanel whenActive="3">contenido 3</TabPanel>
